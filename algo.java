ca84f4ff98f7fe0c9353b3f14f9a4d858cba96fa
Benjamin Scherling <scherlingbenjamin@gmail.com>
2022-10-21 22:09:56 +0200

    update

--- algo.java
+++ algo.java
@@ -1,32 +1,28 @@
-
-
-public class Algo{
-
-
-
-    public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5,80,11,9,8,7,6,5,4};
-        int k = arr[5];
-        double array_divide = Math.ceil(arr.length/2);
-        //int n = arr.length;
-        
-        while(array_divide != k){
-            if(arr[(int)array_divide] <= arr[(int)array_divide -1]){
-                array_divide = Math.ceil(array_divide/2);
-                
-                
-            }
-            else{
-                array_divide = array_divide + Math.ceil(array_divide/2);
+public class algo{
+public static void main(String[] args) {
+    int[] arr = {1,2,3,4,5,6,7,8,9,10,11,12,13,14};
+    int k = 13;
+    double array_divide = (Math.ceil(arr.length/2)-1);
     
-            }
+    while(array_divide != k){
+        if(arr[(int)array_divide] <= arr[(int)array_divide -1]){
+            array_divide = Math.ceil(array_divide/2);
+            System.out.println(array_divide);
             
-    
             
         }
-        System.out.println(k);
-    
-        
-        
+        else{
+            array_divide = array_divide + Math.floor(((arr.length -array_divide)/2));
+            System.out.println(array_divide);
+
         }
+        
+
+        
     }
+    System.out.println(array_divide);
+
+    
+    
+    }
+}
